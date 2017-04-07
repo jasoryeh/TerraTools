602ed166456240937ef2cb46ebd3fc21529ead85
Jason <jasoryeh@gmail.com>
2017-04-06 20:50:19 -0700

    Update plugin.yml and add /discord and /website cmd.

--- CustomCommands/src/tt/customcommands/commandcore.java
+++ CustomCommands/src/tt/customcommands/commandcore.java
@@ -11,12 +11,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class commandcore extends JavaPlugin{
   public void onEnable() {
-	  getLogger().info(ChatColor.YELLOW + "[InputCore] Starting up...");
+	  getLogger().info(ChatColor.YELLOW + "[TerraTools] Starting up...");
 	  getConfig().options().copyDefaults();
 	  saveConfig();
   }
   public void onDisable() {
-	  getLogger().info(ChatColor.YELLOW + "[InputCore] Shutting down...");
+	  getLogger().info(ChatColor.YELLOW + "[TerraTools] Shutting down...");
 	  }
   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 	  Player player = (Player) sender;
@@ -30,23 +30,30 @@ public class commandcore extends JavaPlugin{
 		  }
 	  }
 	  if(label != null && label.equalsIgnoreCase("idek")){
-		  Player p = player;
 		  if(args.length == 0){
 			  System.out.println("LOL");
-			  p.sendMessage(ChatColor.RED + "Error - use /info discord, or website.");
+			  player.sendMessage(ChatColor.RED + "Error - use /info discord, or website.");
 		  }else if(args.length == 1){
 			  if(args[0] != null && args[0].equalsIgnoreCase("discord")){
 				  String DISCORDSITE = getConfig().getString("DISCORD", "https://discord.gg/G9tvJSu");
-				  p.sendMessage(ChatColor.BLUE + "Join our discord @ " + DISCORDSITE);			  
+				  player.sendMessage(ChatColor.BLUE + "Join our discord @ " + DISCORDSITE);			  
 			  }else if(args[0] != null && args[0].equalsIgnoreCase("website")){
 				  String WEBSITESITE = getConfig().getString("WEBSITE", "https://terramagnum.net");
-				  p.sendMessage(ChatColor.YELLOW + "Our website is here: " + WEBSITESITE);
+				  player.sendMessage(ChatColor.YELLOW + "Our website is here: " + WEBSITESITE);
 			  }else if(args[0] == null){
 				  getLogger().info("null string for args[0]");
 			  }else{
 				  getLogger().info(":P");
 			  } 
 		  }
+	  }
+	  if(label.equalsIgnoreCase("discord")){
+		  String DISCORDSITE = getConfig().getString("DISCORD", "https://discord.gg/G9tvJSu");
+		  player.sendMessage(ChatColor.BLUE + "Join our discord @ " + DISCORDSITE);
+	  }
+	  if(label.equalsIgnoreCase("website")){
+		  String WEBSITESITE = getConfig().getString("WEBSITE", "https://terramagnum.net");
+		  player.sendMessage(ChatColor.YELLOW + "Our website is here: " + WEBSITESITE);
 	  }
     return true;
   }
